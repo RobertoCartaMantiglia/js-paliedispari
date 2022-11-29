9e36d25a0deb54c83ee7048175ec070c6ea7c9cd
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-29 17:13:45 +0100

    ex palindrome done

--- js/script.js
+++ js/script.js
@@ -4,32 +4,28 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 
+// chiediamo all'utente una parola 
+let userWord = prompt("scrivi una parola");
 
-let string = "";
-let stringReverse = "";
+        //definiamo la funzione
+function isPalindrome(stringa){
+        
+    let wordLength = stringa.length;  //definisco la lunghezza della variabile = alla lunghezza della stringa
+    let palindrom = true;    //definisco una funzione che mi darà il risultato di ciò che sto cercando, parto dal presupposto che tutte le parole siano palindrome così da poter poi invertire la risposa booleana
 
-
-// cerchiamo di vedere una parola come una array; partiamo in questo caso dalla ultima posizione e risaliamo fino alla posizione zero
-// un palindromo restituirà a video i medesimi valori sia visto dall'inizio che dalla fine
-
-
-// qui il ciclo for è al contrario perché stiamo partendo dall'ultima posizione 
-
-function isPalindrome(string){
-
-    let word = string.length;
-
-    for (let i=string.length - 1; i>=0; i--){
-        stringReverse = stringReverse + string[i];
-    }
-    if (stringReverse === string){
-        alert(word("è palindroma"));
-    } 
-    return word;   
+    for (let i=0; i<wordLength /2; i++){          //faccio un ciclo for per scorrere la lunghezza della parola
+        
+        if(stringa[i] != stringa [stringa.length - 1 -i]) {    //se i valori che compongono la stringa son diversi se "letti" in verso opposto allora palindrom= false
+            palindrom = false;
+        }
+    }     
+    return palindrom;   
 
 }
-let userWord = prompt("scrivi una parola");
-console.log(userWord);
+const risultato = isPalindrome (userWord);
+console.log(risultato);
+
+
 
 
 
