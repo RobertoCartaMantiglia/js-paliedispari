2139b1ebba6fd0999ed8913a8213a93d953d4633
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-29 15:48:25 +0100

    trying to make a plaindrome

--- js/script.js
+++ js/script.js
@@ -2,3 +2,29 @@
 //Palidroma
 // Chiedere all’utente di inserire una parola:
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
+
+
+
+
+function isPalindrome (str){
+    //calcoliamo la lunghezza della parola
+    const wordLength = string.length;
+
+    for (let i=0; i < wordLength / 2; i++){
+
+        if(string[i] !== string[wordLength - 1 - i]){
+            alert("non è palindromo");
+        } else {
+            alert("è palindromo");
+        }
+    }
+    return wordLength;
+}
+
+let userWord = prompt("scrivi una parola");
+
+
+
+
